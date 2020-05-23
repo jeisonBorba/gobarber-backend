d69459eddc35a8c5c59e8b77764349fc28115365
jeisonBorba <jeizon.borba@gmail.com>
2020-05-23 15:07:06 -0300

    remove rate limit from upload

--- src/shared/infra/http/middlewares/exceptionHandler.ts
+++ src/shared/infra/http/middlewares/exceptionHandler.ts
@@ -15,6 +15,8 @@ export default function exceptionHandler(
     });
   }
 
+  console.error(error);
+
   return response.status(500).json({
     status: 'error',
     message: 'Internal server error',
